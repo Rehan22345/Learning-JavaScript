4e1c185bb20dcaafe6e32ebd64ca2144c9003cc6
Rehan-Paudel <108277246+Rehan22345@users.noreply.github.com>
2024-09-21 22:48:32 +0545

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,85 @@
+ // Comments are the part of code which don't get executed but is a part of code just to tell other dev bout tthe code reading now 
+ // console.log dispaly or print that result in console of browser 
+// variable , constant
+// variables => variables are container  to store data in them 
+// var , let and const for constaant 
+// var , let 
+// var ko na jane panii hunxa kind of old 
+// let chai modern js ko features ho , used garna paryo instead of var 
+// let name = "Divine";
+// let name = "Js";
+// console.log(name);
+//  name vanni bado wa container ma uha ko name xa 
+// name // arko choti hjr le ferii tei name bata vado banuna mildaina k 
+// let a = "JavaScript";
+// console.log(a)
+//     a = "react JS";
+// console.log(a)
+// make sure to use let and constant's
+// data types hooo
+// 1. Number 
+// 2.Strings 
+// 3. Boolean
+// 4.Null
+// 5. Undefined
+// let isMarried = false;
+// after 10,yrs is married = true
+// register // data halnunxa and create hunxa registered : false after registe true
+// type of kura haruu ho hai tw 
+// let a = 10;
+// let name = "Hello";
+// let ismarried = true;
+// let nulldata = undefined;
+// console.log(typeof nulldata);
+
+
+//  string method 
+// console.log(a.includes("h"));
+
+// jhola ma chai hjr le check garna paryo k vanera vanda sceince kitab xa ki nai vanera if yes true else false
+// console.log(a.indexOf("o"));
+// console.log(a.lastIndexOf("h"))
+// slice n split 
+// length 
+
+//  class ma chai rehan ko postioon k xa vanera sde poxii k aauxa j xa pos j xa tei aauxa 
+//  noted h = 0;
+//  5 = h
+// let fruitname = "Apple";
+// let fruitname2 = "banana";
+// let fruits = ["Apple","Banana"];
+
+//  array methods 
+// console.log(fruits.push("Grapes"));
+// console.log(fruits.pop())
+
+
+// apple 
+//  banana 
+// grapes
+//  pop 
+// let a = "Rehan";
+// console.log(a.toUpperCase())
+// console.log(a)
+
+
+// a = "Rehan";
+ // yedii method use garera string or array ma modification leununxuu vanii copy vanxa direct mutate hudaina 
+
+
+// notes copyy of science
+// yedii hjr lai tyo note ko auta pana chythya man lagyo vannii k garnunxa direct tei lai tah garnuna hola nii tesko arko copy banyera copy baa ko ma garnunxa ni tei ho 
+ // opeartors 
+
+// realtional opeartors == value matra check garxa  , === value + data type pani check garxa
+// logical opeartors 
+// let a = 10;
+// let b = 20;
+// let d = 50;
+// let c = a < b < d;
+// console.log(c);
+
+//  true > d
+//  true > 50
+// 1 > 50
+// true = 1 and false = 0;
